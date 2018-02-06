d8360d422379d7b64233be4fdfdc9ddf53aa6b84
fnsne <watashiwafnsne@gmail.com>
2018-02-06 20:39:57 +0800

    Writing string to integer 3rd times

---
+++ Chapter4/chapter4-1/string_to_integer_3t.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+int main(){
+  char s[10] = "26962869";
+  int length = strlen(s);
+  int n = 0;
+  for( int i = 0; i < length; i++){
+    n *= 10;
+    n += s[i] -'0';
+  }
+  cout << "string to integer : " << n << endl;
+
+  return 0;
+}
